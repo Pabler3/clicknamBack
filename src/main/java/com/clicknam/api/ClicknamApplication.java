f806ba15011b64af1486ee486e210dcfb775206f
VASS\parcas <pabloarcas89@gmail.com>
2024-04-19 19:33:30 +0200

    Initial commit

---
+++ src/main/java/com/clicknam/api/ClicknamApplication.java
@@ -0,0 +1,13 @@
+package com.clicknam.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicknamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicknamApplication.class, args);
+	}
+
+}
